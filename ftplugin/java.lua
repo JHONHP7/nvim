d26dcebb9ffa1bd043d93bf3295dc90a134dd38e
Jhonatan Silva <jhonatan.oliveira.dev@gmail.com>
2026-05-24 14:46:52 -0300

    add: first debugging

---
+++ ftplugin/java.lua
@@ -0,0 +1,73 @@
+local jdtls = require("jdtls")
+
+local home = os.getenv("HOME")
+local mason = vim.fn.stdpath("data") .. "/mason/packages"
+
+local workspace = home
+    .. "/.cache/jdtls/"
+    .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+
+local bundles = {
+  vim.fn.glob(
+    mason
+    .. "/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",
+    true
+  ),
+}
+
+vim.list_extend(
+  bundles,
+  vim.split(
+    vim.fn.glob(
+      mason .. "/java-test/extension/server/*.jar",
+      true
+    ),
+    "\n"
+  )
+)
+
+local config = {
+ cmd = {
+  "java",
+  "-javaagent:" .. home .. "/.config/nvim/lombok/lombok.jar",
+
+  "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+  "-Dosgi.bundles.defaultStartLevel=4",
+  "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    "-Dlog.protocol=true",
+    "-Dlog.level=ALL",
+
+    "-Xms1g",
+
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens",
+    "java.base/java.util=ALL-UNNAMED",
+    "--add-opens",
+    "java.base/java.lang=ALL-UNNAMED",
+
+    "-jar",
+    vim.fn.glob(
+      mason .. "/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"
+    ),
+
+    "-configuration",
+    mason .. "/jdtls/config_linux",
+
+    "-data",
+    workspace,
+  },
+
+  root_dir = require("jdtls.setup").find_root({
+    ".git",
+    "mvnw",
+    "gradlew",
+    "pom.xml",
+    "build.gradle",
+  }),
+
+  init_options = {
+    bundles = bundles,
+  },
+}
+
+jdtls.start_or_attach(config)
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -6,6 +6,7 @@ return {
       require("mason").setup()
     end,
   },
+
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
@@ -13,14 +14,12 @@ return {
       auto_install = false,
     },
   },
+
   {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local mason_path = vim.fn.expand("$HOME/.local/share/nvim/mason")
-      local lombok_path = vim.fn.expand("$HOME/.config/nvim/lombok/lombok.jar")
-      local launcher = vim.fn.glob(mason_path .. "/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar")
 
       vim.lsp.config("ts_ls", { capabilities = capabilities })
       vim.lsp.config("html", { capabilities = capabilities })
@@ -28,6 +27,7 @@ return {
       vim.lsp.config("dockerls", { capabilities = capabilities })
       vim.lsp.config("cssls", { capabilities = capabilities })
       vim.lsp.config("jsonls", { capabilities = capabilities })
+
       vim.lsp.config("yamlls", {
         capabilities = capabilities,
         settings = {
@@ -38,38 +38,25 @@ return {
           },
         },
       })
-      vim.lsp.config("jdtls", {
-        capabilities = capabilities,
-        cmd = {
-          "java",
-          "-javaagent:" .. lombok_path,
-          "-jar", launcher,
-          "-configuration", mason_path .. "/packages/jdtls/config_linux",
-          "-data", vim.fn.expand("$HOME/.local/share/nvim/jdtls-workspace"),
-        },
-        settings = {
-          java = {
-            eclipse = { downloadSources = true },
-            maven = { downloadSources = true },
-            format = { enabled = true },
-          },
-        },
-      })
 
       vim.lsp.enable({
-        "ts_ls", "html", "lua_ls",
-        "dockerls", "cssls",
-        "jsonls", "yamlls", "jdtls",
+        "ts_ls",
+        "html",
+        "lua_ls",
+        "dockerls",
+        "cssls",
+        "jsonls",
+        "yamlls",
       })
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover)
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references)
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
     end,
   },
 }
