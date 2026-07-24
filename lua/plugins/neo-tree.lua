cd4684007ecd721e969501ca4206aeb5658cd05f
Jhonatan Silva <jhonatan.oliveira.dev@gmail.com>
2026-07-24 02:10:00 -0300

    feat: add doc

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,20 +1,23 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("neo-tree").setup({
-      window = { width = 45 },
-      filesystem = {
-        follow_current_file = { enabled = true },
-        hijack_netrw_behavior = "open_current",
-      },
-    })
-    vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
-    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			window = {
+				width = 45,
+				auto_expand_width = true,
+			},
+			filesystem = {
+				follow_current_file = { enabled = true },
+				hijack_netrw_behavior = "open_current",
+			},
+		})
+		vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+	end,
 }
